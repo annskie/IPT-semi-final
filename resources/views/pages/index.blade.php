e995fbd525ea21854db1bebbe054416109e46a47
unknown <chadiegilaugis@gmail.com>
2021-11-20 18:35:37 +0800

    semi final ipt

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="container-fluid">
+    <div class="d-flex flex-wrap justify-content-center">
+        <div class="d-flex flex-row">
+            <img src="https://source.unsplash.com/1000x500/?lamborghini,car" class="img-fluid" alt="connect to internet to view the image">
+        </div>
+
+        <div class="d-flex flex-row">
+            <img src="https://source.unsplash.com/1000x500/?ferrari,car" class="img-fluid" alt="connect to internet to view the image">
+        </div>
+        <div class="d-flex flex-row">
+            <img src="https://source.unsplash.com/1000x500/?wrangler,car" class="img-fluid" alt="connect to internet to view the image">
+        </div>
+
+
+    </div>
+</div>
+
+
+@endsection
